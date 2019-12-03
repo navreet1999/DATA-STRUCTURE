598cb58c1bca1a374a41eae6ff5571eb928f9a71
NAVREET KAUR DHIMAN <44254251+navreet1999@users.noreply.github.com>
2019-12-03 20:11:39 +0530

    Balanced Paraenthesis

---
+++ Balanced paraenthesis.c
@@ -0,0 +1,201 @@
+/*
+
+A bracket is considered to be any one of the following characters: (, ), {, }, [, or ].
+
+Two brackets are considered to be a matched pair if the an opening bracket (i.e., (, [, or {) occurs to the left of a closing bracket (i.e., ), ], or }) of the exact same type. There are three types of matched pairs of brackets: [], {}, and ().
+
+A matching pair of brackets is not balanced if the set of brackets it encloses are not matched. For example, {[(])} is not balanced because the contents in between { and } are not balanced. The pair of square brackets encloses a single, unbalanced opening bracket, (, and the pair of parentheses encloses a single, unbalanced closing square bracket, ].
+
+By this logic, we say a sequence of brackets is balanced if the following conditions are met:
+
+It contains no unmatched brackets.
+The subset of brackets enclosed within the confines of a matched pair of brackets is also a matched pair of brackets.
+Given  strings of brackets, determine whether each sequence of brackets is balanced. If a string is balanced, return YES. Otherwise, return NO.
+
+Function Description
+
+Complete the function isBalanced in the editor below. It must return a string: YES if the sequence is balanced or NO if it is not.
+
+isBalanced has the following parameter(s):
+
+s: a string of brackets
+Input Format
+
+The first line contains a single integer , the number of strings. 
+Each of the next  lines contains a single string , a sequence of brackets.
+
+Constraints
+
+, where  is the length of the sequence.
+All chracters in the sequences ∈ { {, }, (, ), [, ] }.
+Output Format
+
+For each string, return YES or NO.
+
+Sample Input
+
+3
+{[()]}
+{[(])}
+{{[[(())]]}}
+Sample Output
+
+YES
+NO
+YES
+Explanation
+
+The string {[()]} meets both criteria for being a balanced string, so we print YES on a new line.
+The string {[(])} is not balanced because the brackets enclosed by the matched pair { and } are not balanced: [(]).
+The string {{[[(())]]}} meets both criteria for being a balanced string, so we print YES on a new line.*/
+
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+
+
+typedef struct stack
+{
+int data;
+struct stack* next;
+
+}st;
+
+void push(st** top, int data)
+{
+    st* new = (st*)malloc(sizeof(st));
+    
+    new->data = data;
+    new->next = *top;
+    *top = new;
+    
+}
+
+int peek(st* top)
+{
+    return top->data;
+}
+
+int empty(st* top)
+{
+    if(top==NULL)
+    {
+        return 1;
+    }
+    return 0;
+}
+void pop(st** top)
+{
+    
+        st* temp=*top;
+        *top=(*top)->next;
+        free(temp);
+        temp=NULL;    
+}
+
+
+
+
+char* isBalanced(char* values)
+ {
+
+
+    int values_count=strlen(values);
+  
+     st* s=NULL;
+    
+     int p=0;
+    for(int i=0;i<values_count;i++)
+    {
+    
+        char c=' ';
+        if(!empty(s))
+        c=values[s->data];
+        if(empty(s) && (values[i]=='(' || values[i]=='{' || values[i]=='['))
+        push(&s,i);
+        else if(empty(s) && (values[i]==')' || values[i]=='}' || values[i]==']'))
+        {p=1;
+        break;}
+        else if((c=='(' && values[i]==')') || (c=='{' && values[i]=='}') || (c=='[' &&                   values[i]==']'))
+        pop(&s);
+        else if(c=='{' || c=='(' || c=='[')
+        push(&s,i);
+        else
+        {
+           
+           p=1;
+           break;
+        }
+    
+    }
+     if(p==0 && s==NULL)
+           return ("YES");
+       else 
+          return("NO");
+
+}
+
+
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char* t_endptr;
+    char* t_str = readline();
+    int t = strtol(t_str, &t_endptr, 10);
+
+    if (t_endptr == t_str || *t_endptr != '\0') { exit(EXIT_FAILURE); }
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        char* s = readline();
+
+        char* result = isBalanced(s);
+
+        fprintf(fptr, "%s\n", result);
+    }
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) { break; }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') { break; }
+
+        size_t new_length = alloc_length << 1;
+        data = realloc(data, new_length);
+
+        if (!data) { break; }
+
+        alloc_length = new_length;
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+    }
+
+    data = realloc(data, data_length);
+
+    return data;
+}
