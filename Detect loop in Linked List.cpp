ae655b6fff8ab4a36228ddce92c79e44ed717cfe
NAVREET KAUR DHIMAN <44254251+navreet1999@users.noreply.github.com>
2020-01-22 21:06:58 +0530

    detect loop in linked list

---
+++ Detect loop in Linked List.cpp
@@ -0,0 +1,57 @@
+/*
+Given a linked list of N nodes. The task is to check if the the linked list has a loop. Linked list can contain self loop.
+
+Input:
+First line of input contains number of testcases T. For each testcase, first line of input contains length of linked list and next line contains the data of linked list.
+
+Output:
+For each testcase, print "True", if linked list contains loop, else print "False".
+
+User Task:
+The task is to complete the function detectloop() which contains reference to the head as only argument. This function should return 1 if linked list contains loop, else return 0.
+
+Constraints:
+1 <= T <= 50
+1 <= N <= 300
+
+Example:
+Input:
+2
+3
+1 3 4
+2
+4
+1 8 3 4
+0
+
+Output:
+True
+False*/
+
+/*
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+
+*/
+int detectloop(Node *head) {
+    
+    Node *slow=head;
+    Node *fast=slow->next;
+    
+    while(slow && fast && fast->next)
+    {
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast)
+        {
+           // cout<<"True";
+            return 1;
+        }
+    }
+    return 0;
+}
